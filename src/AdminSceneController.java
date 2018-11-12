4105615633ab5ab7171d1ff10212f3baa86b9715
anik <anikhossain44@gmail.com>
2018-11-12 13:58:40 -0500

    Addded admin login by Yassine TH 11/12

---
+++ src/AdminSceneController.java
@@ -0,0 +1,75 @@
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AdminSceneController {
+
+
+
+        @FXML
+        private JFXButton addM;
+
+        @FXML
+        private JFXButton AddC;
+
+        @FXML
+        private JFXButton AddTime;
+
+        @FXML
+        private JFXButton DeleteM;
+
+        @FXML
+        private JFXButton DeleteC;
+
+        @FXML
+        private JFXButton RemoveTime;
+
+        @FXML
+        private JFXButton btnHome;
+
+
+        @FXML
+        void btnaddMClicked (ActionEvent event){
+
+        }
+
+         @FXML
+        void btnAddCClicked(ActionEvent event) {}
+
+        @FXML
+        void btnAddTimeClicked(ActionEvent event) {}
+
+
+        @FXML
+        void btnDeleteMClicked(ActionEvent event) {}
+
+        @FXML
+        void btnDeleteCClicked(ActionEvent event) {}
+
+
+        @FXML
+        void btnRemoveTimeClicked(ActionEvent event) {}
+
+
+        @FXML
+        void btnHomeClicked(ActionEvent event) {
+            try{
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("MovieApp.fxml"));
+                Stage stage = (Stage) btnHome.getScene().getWindow();
+
+                Scene scene = new Scene(loader.load());
+                stage.setScene(scene);
+
+
+                }catch (Exception e){
+
+                     }
+        }
+
+
+
+
+}
